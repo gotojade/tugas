73bd0ff9c16c4c8ec87313fe8cb49c29ee2fb2d4
gotojade <gotojade@yahoo.com>
2024-08-29 12:17:50 +0700

    perubahan

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
-// // ini javascript untuk form dan isi
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting
+// untuk awal masuk nama
+let name = prompt ("Hai, dengan siapa ini?","");
+document.getElementById("nametamu").innerText=name
 
+
+// Untuk tabel dan isian
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    // Tabel input
+    // input data
     const name = document.getElementById('name').value;
     const dob = document.getElementById('dob').value;
     const gender = document.querySelector('input[name="gender"]:checked').value;
     const message = document.getElementById('message').value;
 
-    const currentTime = new Date().toString();
+    // tabel hasil
+    // untuk waktu
+    const currentTime = new Date().toLocaleString();
 
+    // hasil inputan
     document.getElementById('currentTime').textContent = `Current time: ${currentTime}`;
     document.getElementById('outputName').textContent = `Nama: ${name}`;
     document.getElementById('outputDob').textContent = `Tanggal Lahir: ${dob}`;
     document.getElementById('outputGender').textContent = `Jenis Kelamin: ${gender}`;
     document.getElementById('outputMessage').textContent = `Pesan: ${message}`;
 });
+
+// untuk banner slide
+
