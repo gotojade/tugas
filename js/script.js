8249b5e7ecff78fbd5a2bb9995c47ec21d7c4ee5
gotojade <gotojade@yahoo.com>
2024-08-29 16:37:06 +0700

    ganti

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     // Tabel input
     // input data
     const name = document.getElementById('name').value;
-    const dob = document.getElementById('dob').value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
     const message = document.getElementById('message').value;
 
     // tabel hasil
@@ -19,12 +19,30 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     const currentTime = new Date().toLocaleString();
 
     // hasil inputan
-    document.getElementById('currentTime').textContent = `Current time: ${currentTime}`;
-    document.getElementById('outputName').textContent = `Nama: ${name}`;
-    document.getElementById('outputDob').textContent = `Tanggal Lahir: ${dob}`;
-    document.getElementById('outputGender').textContent = `Jenis Kelamin: ${gender}`;
-    document.getElementById('outputMessage').textContent = `Pesan: ${message}`;
+    document.getElementById('currentTime').textContent = `Current time : ${currentTime}`;
+    document.getElementById('outputName').textContent = `Nama : ${name}`;
+    document.getElementById('outputemail').textContent = `Email : ${email}`;
+    document.getElementById('outputphone').textContent = `Telepon/HP : ${phone}`;
+    document.getElementById('outputMessage').textContent = `Pesan : ${message}`;
 });
 
 // untuk banner slide
+let indexSlide=1;
+showBanner(1);
 
+function nextSlide(n) {
+
+}
+
+function showBanner(indexBanner){
+    let listImage=document.getElementsByClassName('banner banner');
+    let index = 0;
+    while (index < listImage) {
+        listImage [index].style.display='none';
+        index++;
+
+    }
+
+    listImage[indexBanner=-1].style.display='block'
+    console.log(listImage);
+}
